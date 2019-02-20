b19972e4ac30168240d2c61ea1696a2170bc0c78
Adam Johnson <adam.johnson@pioneer.com>
2019-02-20 14:17:50 -0600

    TOP, START AT, and inner ORDER BY fixes

--- EFCore.Ase/Internal/AseQuerySqlGenerator.cs
+++ EFCore.Ase/Internal/AseQuerySqlGenerator.cs
@@ -1,7 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore.Query.Expressions;
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Query.Expressions;
 using Microsoft.EntityFrameworkCore.Query.Sql;
 using Microsoft.EntityFrameworkCore.Storage;
+using Remotion.Linq.Clauses;
+using Remotion.Linq.Clauses.Expressions;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 
 namespace EntityFrameworkCore.Ase.Internal
@@ -9,6 +16,7 @@ namespace EntityFrameworkCore.Ase.Internal
     internal class AseQuerySqlGenerator : DefaultQuerySqlGenerator
     {
         private readonly Lazy<IRelationalCommandBuilder> _relationalCommandBuilder2;
+        private int _levels;
 
         public AseQuerySqlGenerator(QuerySqlGeneratorDependencies dependencies, SelectExpression selectExpression)
             : base(dependencies, selectExpression)
@@ -19,15 +27,48 @@ namespace EntityFrameworkCore.Ase.Internal
 
         protected override void GenerateTop(SelectExpression selectExpression)
         {
-            if (selectExpression.Limit != null
-                && selectExpression.Offset == null)
+            if (selectExpression.Limit != null)
             {
                 _relationalCommandBuilder2.Value.Append("TOP ");
 
-                Visit(selectExpression.Limit);
+                _relationalCommandBuilder2.Value.Append(ParameterValues[selectExpression.Limit.ToString()]);
 
                 _relationalCommandBuilder2.Value.Append(" ");
+
+                if (selectExpression.Offset != null)
+                {
+                    _relationalCommandBuilder2.Value.Append("START AT ");
+
+                    _relationalCommandBuilder2.Value.Append(ParameterValues[selectExpression.Offset.ToString()]);
+
+                    _relationalCommandBuilder2.Value.Append(" ");
+                }
             }
+        }
+
+        public override IRelationalCommand GenerateSql(IReadOnlyDictionary<string, object> parameterValues)
+        {
+            _levels = 0;
+            var r = base.GenerateSql(parameterValues);
+            Debug.WriteLine(r.CommandText);
+            return r;
+        }
+        
+        public override Expression VisitSelect(SelectExpression selectExpression)
+        {
+            if (++_levels > 1)
+            {
+                selectExpression.ClearOrderBy();
+            }
+
+            var v = base.VisitSelect(selectExpression);
+
+            _levels--;
+            return v;
+        }
+    
+        protected override void GenerateLimitOffset(SelectExpression selectExpression)
+        {
         }
     }
 }
